37e6084441c29d564e3866ce73401335de6f6fd6
Luis Parrado <luisprmat@gmail.com>
2024-04-29 16:06:27 -0500

    Install TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [forms, typography],
+};
